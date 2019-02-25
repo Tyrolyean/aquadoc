c221cb08a21bb548c68af9b91ff636bc7c16a21c
Jack <jackneuner@tsn.at>
2019-02-25 11:12:01 +0100

    Ultrasonic sensor basically working but not really
    Signed-off-by: Jack <jackneuner@tsn.at>

--- psoc/psoc/Ultrasonic_test.cydsn/codegentemp/Ultrasonic_test.ctl
+++ psoc/psoc/Ultrasonic_test.cydsn/codegentemp/Ultrasonic_test.ctl
@@ -1,6 +1,6 @@
 -- ======================================================================
 -- Ultrasonic_test.ctl generated from Ultrasonic_test
--- 02/18/2019 at 10:48
+-- 02/25/2019 at 10:18
 -- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 -- ======================================================================
 
--- psoc/psoc/Ultrasonic_test.cydsn/main.c
+++ psoc/psoc/Ultrasonic_test.cydsn/main.c
@@ -11,6 +11,34 @@
 */
 #include "project.h"
 #include <unistd.h>
+#include <stdio.h>
+
+size_t get_hc_sr04(){
+    
+    /* If the timeout is reached, the sensor is 
+     * assumed to have been disconnected
+     */
+    
+    #define ERRROR_CORRECTION 500
+    
+    #define TIMEOUT 50000
+    
+    /* Place your application code here. */
+    Trigger_Impuls_Write(1);
+    CyDelayUs(10);
+    Trigger_Impuls_Write(0);
+    
+    for(int i = 0; i < TIMEOUT && !Echo_Input_Read(); i++){
+        CyDelayUs(1);
+    }
+    
+    //ultimate Baustelle
+    size_t counter;
+    for(counter = 0;Echo_Input_Read() == 1 && counter <= TIMEOUT; counter ++){
+        CyDelayUs(1);
+    }
+    return counter + ERRROR_CORRECTION;
+}
 
 int main(void)
 {
@@ -19,13 +47,17 @@ int main(void)
     UART_1_Start();
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    UART_1_PutString("INIT\r\n");
 
     for(;;)
     {
-        /* Place your application code here. */
-        Trigger_Impuls_Write(1);
-        usleep(10);
-        Trigger_Impuls_Write(0);
+        size_t cm =  get_hc_sr04() / 58;
+        char str [20];
+        snprintf(str, sizeof(str), "Distance: %icm\r\n", cm);
+        
+        UART_1_PutString(str);
+        
+        //ultimate Baustelle
         
         
         
