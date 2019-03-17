75a01f021b644c46251d91f97fa9714f485df0bb
Tyrolyean <tyrolyean@tyrolyean.net>
2019-03-17 03:53:46 +0100

    Added new stuff!
    Signed-off-by: Tyrolyean <tyrolyean@tyrolyean.net>

--- psoc/psoc/Ultrasonic_test.cydsn/hcsr04.c
+++ psoc/psoc/Ultrasonic_test.cydsn/hcsr04.c
@@ -1,56 +1,60 @@
-
+/* Busy wait implementation of an HC-SR04
+ *
+ */
 #include "hcsr04.h"
 #include "project.h"
 
 uint32_t get_hc_sr04(){
-    
-    /* If the timeout is reached, the sensor is 
-     * assumed to have been disconnected
-     */
-    
-    
-    /* We assumed that a linear function correcting the
-     * measurements would be enough. We took measurements at 1m and 2m and got as
-     * results the values .36m and .72m respectively. We then perceeded to
-     * put a linear function through the points (.36, 100) and (.72, 100)
-     * which resulted in a slope of 2.77777 or 25/9 and an offset of 0.
-     * Therefore we assumed an only linear distortion. We took further measurements
-     * and got errors in the range of 1-3% of the real distances, which was deemed enough.
-     */
-    #define CORRECTION_FACTOR ((double)25.0/9.0)
-    
-    #define TIMEOUT 50000
-    
-    /* Avoid further distortion through triggered interrupts. */
-    CyGlobalIntDisable;
-    
-    /* Place your application code here. */
-    Trigger_Impuls_Write(1);
-    CyDelayUs(10);
-    Trigger_Impuls_Write(0);
-    
-    
-    for(int i = 0; i < TIMEOUT && !Echo_Input_Read(); i++){
-        CyDelayUs(1);
-    }
-    
-    uint32_t counter;
-    
-    for(counter = 0;Echo_Input_Read() == 1 && counter <= TIMEOUT; counter ++){
-        CyDelayUs(1);
-    }
-    CyGlobalIntEnable;
-    
-    /* This statement performes an integer to float converstion and an
-     * float to integer conversion. The calculation itself happens as
-     * (double precision) float.
-     */
-    return (counter * CORRECTION_FACTOR);
+	
+	
+	/* We assumed that a linear function correcting the
+	 * measurements would be enough. We took measurements at 1m and 2m and
+	 * got as results the values .36m and .72m respectively. We then 
+	 * perceeded to put a linear function through the points (.36, 100) and
+	 * (.72, 100) which resulted in a slope of 2.77777 or 25/9 and an offset
+	 * of 0. Therefore we assumed an only linear distortion. We took further 
+	 * measurements and got errors in the range of 1-3% of the real
+	 * distances, which was deemed enough.
+	 */
+	#define CORRECTION_FACTOR ((double)25.0/9.0)
+	
+	/* If the timeout is reached, the sensor is 
+	 * assumed to have been disconnected
+	 */	
+	
+	#define TIMEOUT 50000
+	
+	/* Avoid further distortion through triggered interrupts. */
+	CyGlobalIntDisable;
+	
+	/* Place your application code here. */
+	Trigger_Impuls_Write(1);
+	CyDelayUs(10);
+	Trigger_Impuls_Write(0);
+	
+	
+	for(int i = 0; i < TIMEOUT && !Echo_Input_Read(); i++){
+		CyDelayUs(1);
+	}
+	
+	uint32_t counter;
+	
+	for(counter = 0;Echo_Input_Read() == 1 && counter <= TIMEOUT; counter ++){
+		CyDelayUs(1);
+	}
+
+	CyGlobalIntEnable;
+	
+	/* This statement performes an integer to float converstion and an
+	 * float to integer conversion. The calculation itself happens as
+	 * (double precision) float.
+	 */
+	return (counter * CORRECTION_FACTOR);
 }
 
 uint32_t timer_us_hcsr04 = 0;
 
 int init_hcsr04(){
     
-    return 0;
+	return 0;
 }
--- psoc/psoc/Ultrasonic_test.cydsn/main.c
+++ psoc/psoc/Ultrasonic_test.cydsn/main.c
@@ -1,43 +1,25 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include "project.h"
 #include <unistd.h>
 #include <stdio.h>
 
 #include "hcsr04.h"
 
-int main(void)
-{
-    CyGlobalIntEnable; /* Enable global interrupts. */
-    
-    UART_1_Start();
+int main(void){
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    UART_1_PutString("INIT\r\n");
+	CyGlobalIntEnable; /* Enable global interrupts. */
+	
+	UART_1_Start();
+	
+	/* Place your initialization/startup code here (e.g. MyInst_Start()) */
+	UART_1_PutString("INIT\r\n");
+	
+	for(;;){
 
-    for(;;)
-    {
-        size_t cm =  get_hc_sr04() / 58;
-        char str [20];
-        snprintf(str, sizeof(str), "Distance: %icm\r\n", cm);
-        
-        UART_1_PutString(str);
-        
-        //ultimate Baustelle
-        
-        
-        
-        
-    }
+		size_t cm =  get_hc_sr04() / 58;
+		char str [20];
+		snprintf(str, sizeof(str), "Distance: %icm\r\n", cm);
+	
+		UART_1_PutString(str);
+	
+	}
 }
-
-/* [] END OF FILE */
